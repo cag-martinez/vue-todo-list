b5c7cab76967502fea1b7f5adbf64295fd46b206
Christian Gomez Martinez <gomez9289@gmail.com>
2021-01-23 09:10:49 -0500

    added input components but having trouble adding to the todo list and showing on the screen

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,20 +1,40 @@
 <template>
-    <div>
-        <form>
-        <input type="text" name="title" placeholder="add to-do" >
-        <input type="submit" value="Submit" class="button" >
-        </form>
-    </div>
+  <div>
+    <form @submit.prevent="$input">
+      <input type="text" v-model="title" name="title" placeholder="add to-do" />
+      <input type="submit" value="Submit" class="button" />
+    </form>
+  </div>
 </template>
 
 <script>
+// Importing NPM pakage uuid
+import uuid from "uuid";
+
 export default {
-name: "Input"    
-}
+  name: "Input",
+  data() {
+    return {
+      title: ""
+    }
+  },
+  methods: {
+    Input() {
+      const todos = {
+        // Calling on uuid package
+        id: uuid(),
+        title: this.title,
+        completed: false
+      }
+      // sending data to the parent
+      this.$emit("add-todo", todos);
+    }
+  }
+};
 </script>
 
 <style scoped>
 form {
-    align-items: flex;
+  display: flex;
 }
 </style>
