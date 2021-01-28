98d6c8cecffb09a06f9e15241193b8a7b999c4fd
Christian Gomez Martinez <gomez9289@gmail.com>
2021-01-27 22:36:51 -0500

    added some styling to my form

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <form @submit.prevent="addTodo">
-      <input type="text" v-model='title' name="title" placeholder="add to-do" />
+      <input type="text" v-model="title" name="title" placeholder="add to-do" />
       <input type="submit" value="Submit" class="button" />
     </form>
   </div>
@@ -16,7 +16,7 @@ export default {
   data() {
     return {
       title: ""
-    }
+    };
   },
   methods: {
     addTodo(e) {
@@ -26,11 +26,11 @@ export default {
         id: uuid.v4(),
         title: this.title,
         completed: false
-      }
+      };
       // sending data to the parent
       this.$emit("add-todo", newTodo);
 
-      this.title="";
+      this.title = "";
     }
   }
 };
@@ -38,6 +38,17 @@ export default {
 
 <style scoped>
 form {
-  display: flex;
+  display: inline-block;
+  
+}
+input [type="text"] {
+  flex: 2;
+  padding: 5px;
+}
+
+input [type="submit"] {
+  flex: 2;
+  padding: 5px;
+  border: 5px;
 }
 </style>
